2f1a979af9afef2180849ee74dde114964b5ca7d
muganzacoders-eng <muganzacoders@gmail.com>
2025-10-03 18:27:58 +0300

    Update change.js

--- change.js
+++ change.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types'; // Add this import
+import PropTypes from 'prop-types';
 import { Navigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../contexts/AuthContext';
 
@@ -34,4 +34,5 @@ export function PublicRoute({ children }) {
 
 PublicRoute.propTypes = {
   children: PropTypes.node.isRequired,
+
 };
